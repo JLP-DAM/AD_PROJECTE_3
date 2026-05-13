c148ba3582e6bbea730be9d4f0f353f9063eba5a
JLP-DAM <2223_jan.lazaro@iticbcn.cat>
2026-05-13 19:55:54 +0200

    fixes #6

---
+++ projecte3/src/main/java/com/ra5/projecte3/dto/UserRequestDTO.java
@@ -0,0 +1,94 @@
+package com.ra5.projecte3.dto;
+
+import java.time.LocalDateTime;
+
+import com.ra5.projecte3.model.Role;
+
+public class UserRequestDTO {
+    private String email;
+    private String username;
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private Role role;
+    private String observations;
+    private String grade;
+
+    public UserRequestDTO() {
+
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getObservations() {
+        return observations;
+    }
+
+    public void setObservations(String observations) {
+        this.observations = observations;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+
+    @Override
+    public String toString() {
+        return "UserRequestDTO [email=" + email + ", username=" + username + ", firstName=" + firstName + ", lastName="
+                + lastName + ", password=" + password + ", role=" + role + ", observations=" + observations + ", grade="
+                + grade + "]";
+    }    
+}
+
---
+++ projecte3/src/main/java/com/ra5/projecte3/dto/UserResponseDTO.java
@@ -0,0 +1,78 @@
+package com.ra5.projecte3.dto;
+
+import java.time.LocalDateTime;
+
+import com.ra5.projecte3.model.Role;
+
+public class UserResponseDTO {
+    private String id;
+    private String email;
+    private String username;
+
+    private String firstName;
+    private String lastName;
+    private Role role;
+    private LocalDateTime dataCreated;
+    private AcademicProfileDTO academicProfile;
+    
+    public UserResponseDTO() {
+        
+    }
+
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public Role getRole() {
+        return role;
+    }
+    public void setRole(Role role) {
+        this.role = role;
+    }
+    public LocalDateTime getDataCreated() {
+        return dataCreated;
+    }
+    public void setDataCreated(LocalDateTime dataCreated) {
+        this.dataCreated = dataCreated;
+    }
+    public AcademicProfileDTO getAcademicProfile() {
+        return academicProfile;
+    }
+    public void setAcademicProfile(AcademicProfileDTO academicProfile) {
+        this.academicProfile = academicProfile;
+    }
+    @Override
+    public String toString() {
+        return "UserResponseDTO [id=" + id + ", email=" + email + ", username=" + username + ", firstName=" + firstName
+                + ", lastName=" + lastName + ", role=" + role + ", dataCreated=" + dataCreated + ", academicProfile="
+                + academicProfile + "]";
+    }
+
+}
+
